287a723e5a34d0f13c7eb08f3b06f477fde8d75e
Alex <alexander.harrison@berkeley.edu>
2026-05-06 09:42:51 -0700

    Bluelight filter shader

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # retroarch-nightlight
+Add `nightlight.glslp` to the directory with your other Retroarch presets, and add `shaders/color/nightlight.glsl", preserving the folder structure. Enable it using the preset (`nightlight.glslp`), not by trying to add the shaderfile itself. 
+
---
+++ shaders/color/nightlight.glsl
@@ -0,0 +1,115 @@
+/*
+   Simple blue-light reduction shader for RetroArch
+   Written to match the bundled AmberELEC/RetroArch GLSL style.
+*/
+
+// Compatibility #ifdefs needed for parameters
+#ifdef GL_ES
+#define COMPAT_PRECISION mediump
+#else
+#define COMPAT_PRECISION
+#endif
+
+// Parameters exposed to RetroArch's Shader Parameters menu
+#pragma parameter red_level   "Red Level"   1.00 0.00 2.00 0.01
+#pragma parameter green_level "Green Level" 0.92 0.00 2.00 0.01
+#pragma parameter blue_level  "Blue Level"  0.72 0.00 2.00 0.01
+
+#ifdef PARAMETER_UNIFORM
+uniform COMPAT_PRECISION float red_level;
+uniform COMPAT_PRECISION float green_level;
+uniform COMPAT_PRECISION float blue_level;
+#else
+#define red_level   1.00
+#define green_level 0.92
+#define blue_level  0.72
+#endif
+
+#if defined(VERTEX)
+
+#if __VERSION__ >= 130
+#define COMPAT_VARYING out
+#define COMPAT_ATTRIBUTE in
+#define COMPAT_TEXTURE texture
+#else
+#define COMPAT_VARYING varying
+#define COMPAT_ATTRIBUTE attribute
+#define COMPAT_TEXTURE texture2D
+#endif
+
+#ifdef GL_ES
+#define COMPAT_PRECISION mediump
+#else
+#define COMPAT_PRECISION
+#endif
+
+COMPAT_ATTRIBUTE vec4 VertexCoord;
+COMPAT_ATTRIBUTE vec4 COLOR;
+COMPAT_ATTRIBUTE vec4 TexCoord;
+COMPAT_VARYING vec4 COL0;
+COMPAT_VARYING vec4 TEX0;
+
+uniform mat4 MVPMatrix;
+uniform COMPAT_PRECISION int FrameDirection;
+uniform COMPAT_PRECISION int FrameCount;
+uniform COMPAT_PRECISION vec2 OutputSize;
+uniform COMPAT_PRECISION vec2 TextureSize;
+uniform COMPAT_PRECISION vec2 InputSize;
+
+void main()
+{
+    gl_Position = MVPMatrix * VertexCoord;
+    COL0 = COLOR;
+    TEX0.xy = TexCoord.xy;
+}
+
+#elif defined(FRAGMENT)
+
+#if __VERSION__ >= 130
+#define COMPAT_VARYING in
+#define COMPAT_TEXTURE texture
+out vec4 FragColor;
+#else
+#define COMPAT_VARYING varying
+#define FragColor gl_FragColor
+#define COMPAT_TEXTURE texture2D
+#endif
+
+#ifdef GL_ES
+#ifdef GL_FRAGMENT_PRECISION_HIGH
+precision highp float;
+#else
+precision mediump float;
+#endif
+#define COMPAT_PRECISION mediump
+#else
+#define COMPAT_PRECISION
+#endif
+
+uniform COMPAT_PRECISION int FrameDirection;
+uniform COMPAT_PRECISION int FrameCount;
+uniform COMPAT_PRECISION vec2 OutputSize;
+uniform COMPAT_PRECISION vec2 TextureSize;
+uniform COMPAT_PRECISION vec2 InputSize;
+uniform sampler2D Texture;
+COMPAT_VARYING vec4 TEX0;
+
+// compatibility #defines
+#define Source Texture
+#define vTexCoord TEX0.xy
+#define texture(c, d) COMPAT_TEXTURE(c, d)
+#define SourceSize vec4(TextureSize, 1.0 / TextureSize)
+#define outsize vec4(OutputSize, 1.0 / OutputSize)
+
+void main()
+{
+    vec4 c = texture(Source, vTexCoord);
+
+    c.r *= red_level;
+    c.g *= green_level;
+    c.b *= blue_level;
+
+    FragColor = clamp(c, 0.0, 1.0);
+}
+#endif
+
